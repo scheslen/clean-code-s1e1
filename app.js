107019e9a2d5de2595603b4846b5d889dad22f1b
scheslen <antgalin@mail.ru>
2024-12-12 13:49:27 +0300

    fix(basic-3.1, basic-3.2, basic-3.3): use meaningful, short and BEM names

--- app.js
+++ app.js
@@ -8,9 +8,9 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("new-task");//Add a new task.
+var taskInput=document.getElementById("new__task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incompleteTasks
+var incompleteTaskHolder=document.getElementById("incomplete");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
 
 //New task list item
@@ -54,7 +54,7 @@ var createNewTaskElement=function(taskString){
 
 var addTask=function(){
   console.log("Add Task...");
-  //Create a new list item with the text from the #new-task:
+  //Create a new list item with the text from the #new__task:
   if (!taskInput.value) return;
   var listItem=createNewTaskElement(taskInput.value);
 
