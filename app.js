fb3eff8989ccaa0428f456165f44b311cb5159d6
scheslen <antgalin@mail.ru>
2024-12-12 11:18:28 +0300

    fix(basic-1.2): use lowercase

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //Document is the DOM can be accessed in the console with document.window.
-// Tree is from the top, html, body, p etc.
+// Tree is from the  , html, body, p etc.
 
 //Problem: User interaction does not provide the correct results.
 //Solution: Add interactivity so the user can manage daily tasks.
@@ -10,7 +10,7 @@
 
 var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
+var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
 
 
