91a26b3ae5de0f609d49a4c030fd0d9381c055f0
scheslen <antgalin@mail.ru>
2024-12-12 15:55:13 +0300

    refactor(advanced 2.1): use BEM notation

--- app.js
+++ app.js
@@ -10,8 +10,8 @@
 
 var taskInput=document.getElementById("new__task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incomplete");//ul of #incompleteTasks
-var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
+var incompleteTaskHolder=document.getElementById("incomplete__wrapper");//ul of #incompleteTasks
+var completedTasksHolder=document.getElementById("completed__wrapper");//completed-tasks
 
 //New task list item
 var createNewTaskElement=function(taskString){
